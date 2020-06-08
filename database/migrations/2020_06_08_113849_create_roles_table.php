55506604c690776bddeadb8cd80adaeb699a0a94
Amanneville <61731588+Amanneville@users.noreply.github.com>
2020-06-08 17:09:14 +0200

    Import DB & SEED

---
+++ database/migrations/2020_06_08_113849_create_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('statut');
+            $table->timestamps();
+<<<<<<< HEAD
+
+            $table->foreign('id')->references('role_id')->on('users');
+=======
+>>>>>>> 269cebc8e9095e01fa3b32af6b136acafd32ab40
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
