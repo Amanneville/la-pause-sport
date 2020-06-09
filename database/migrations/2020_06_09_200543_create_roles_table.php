7fe3a6bb5d5166eb52631e1bb21404190aa5f774
Friedkin <paulbisserier@gmail.com>
2020-06-09 22:32:46 +0200

    modification table roles

--- database/migrations/2020_06_09_200543_create_roles_table.php
+++ database/migrations/2020_06_09_200543_create_roles_table.php
@@ -15,6 +15,9 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('admin');
+            $table->string('coach');
+            $table->string('eleve');
             $table->timestamps();
         });
     }
