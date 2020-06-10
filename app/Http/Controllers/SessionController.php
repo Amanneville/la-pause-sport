1a20882fde2fa23ee7721c3e50e5345f7cd14175
Amanneville <61731588+Amanneville@users.noreply.github.com>
2020-06-10 20:32:57 +0200

    Activ. Auth - Models + Controllers + View - Récup. List-Sessions

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Session;
 
+use App\Model\Session;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
     public function index()
     {
-        return view ('session.index');
+       return view ('session.index');
     }
 }
