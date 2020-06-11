326f272cff699d389e9ed2dd7e53357a19a22b45
Friedkin <paulbisserier@gmail.com>
2020-06-11 15:30:21 +0200

    Ajout SessionFactory

--- database/factories/SessionFactory.php
+++ database/factories/SessionFactory.php
@@ -5,8 +5,24 @@
 use App\Session;
 use Faker\Generator as Faker;
 
+// php artisan tinker
+// factory(App\Session::class, 15)->create()
+
+
 $factory->define(\App\Session::class, function (Faker $faker) {
     return [
-
+            'id_sport' => $faker->numberBetween(1,4),
+            'heure_debut' => now(),
+            'heure_fin' => now(),
+            'date' => $faker->date('Y-m-d'),
+            'adresse' => $faker->address,
+            'ville' => $faker->city,
+            'code_postal' => $faker->postcode,
+            'niveau' => $faker->numberBetween(1,4),
+            'nb_max_participants' => $faker->numberBetween(1,15),
+            'prix' => $faker->numberBetween(10,50),
+            'note'=> $faker->numberBetween(1,10),
+            'chat_id' => $faker->numberBetween(1,400),
     ];
+
 });
