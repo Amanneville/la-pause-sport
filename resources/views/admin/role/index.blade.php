25a26ff24a1726894f61e1833aacdfa27cf5a9ca
kaius <cesarkpro@gmail.com>
2020-06-17 15:06:45 +0200

    nouvelle version

--- resources/views/admin/role/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-@section('content')
-
-VUE DE LA PAGE ROLE
-<br> accès ADMIN
-<br> test 3
-
-@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 animate__animated animate__fadeInDown">
+                <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+                <h2>{{ $user->name }} modification photo de profil</h2>
+                <form enctype="multipart/form-data" action="/profile" method="POST">
+                    <label>Mettre à jour votre image de profil</label>
+                    <input type="file" name="avatar">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="submit" class="pull-right btn btn-sm btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
