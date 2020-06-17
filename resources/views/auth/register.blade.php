6d416dd6b9dd9e18e5804094e7f3fe8ccc302369
kaius <cesarkpro@gmail.com>
2020-06-17 15:49:01 +0200

    nouvelle version add 3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -238,34 +238,29 @@
                                 @enderror
                             </div>
                         </div>
-                        Je veux donner des cours
 
-                        <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Membre ou Coach?') }}</label>
-                        <div class="col-md-6">
-                            <select id="role" class="form-control @error('role') is-invalid @enderror" name="role">
-                                <option value="0" selected>Je veux juste pratiquer du sport</option>
-                                <option value="1">Je veux pratiquer du sport et coacher</option>
+                        <div class="form-group row mt-5">
 
-                            </select>
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Membre ou Coach?') }}</label>
+                            <div class="col-md-6">
+                                <select id="role" class="form-control @error('role') is-invalid @enderror" name="role">
+                                    <option value="0" selected>Je veux juste pratiquer du sport</option>
+                                    <option value="1">Je veux pratiquer du sport et coacher</option>
+
+                                </select>
+                            </div>
+                        </div>
+
+
                             @error('role')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="invalid-feedback " role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                             @enderror
                         </div>
 
 
-
-
-
-
-
-
-
-
-
-
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
